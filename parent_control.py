67ae01198f5f7afd64f0fefa6c7af7b6b67ebe1e
Ramprasad Velavarthipati <rampvm25@gmail.com>
2016-04-05 09:21:52 +1000

    Adding Parent Control

--- parent_control.py
+++ parent_control.py
@@ -22,24 +22,66 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
-from ryu.lib.packet import ipv4
-from ryu.lib.packet import udp 
+from ryu.lib.packet import udp
 
-from ryu.lib.dnslib.dns import DNSRecord
-from ryu.lib.dnslib.digparser import DigParser
+import binascii
+
+import ConfigParser
+import os
+import time
+import datetime
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 
+child_mac= []
+blocklist = []
+email = []
 
-class SimpleSwitchPC(app_manager.RyuApp):
+
+class SimpleSwitchPC13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitchPC, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
+        super(SimpleSwitchPC13, self).__init__(*args, **kwargs)
 
+	print ("<!DOCTYPE html>")
+	print ("<html>")
+	print ("<head>")
+	print ("<title>Zodiac Logs</title>")
+	print ("</head>")
+	print ("<body>")
+	print ("<h1>Zodiac Logs</h1>")
+	print ("")
+
+
+        self.mac_to_port = {}
+	
+	config = ConfigParser.ConfigParser ()
+	config.read ("conf.ini")
+	
+	temp = config.get('Configuration','email')
+	emails = temp.split(',')
+	for x in emails:
+		email.append(x)
+
+	
+	temp = config.get('Configuration','child_mac')
+	macs = temp.split(',')
+	for mac in macs:
+		child_mac.append(mac)
+
+	
+	temp = config.get('Configuration','blocklist')
+	sites = temp.split(',')
+	for site in sites:
+		blocklist.append(site)
+
+ 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-	print("DVR In switch_features_handler")
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -56,8 +98,12 @@ class SimpleSwitchPC(app_manager.RyuApp):
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
+	match = parser.OFPMatch(udp_dst=53)
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_MAX)]
+        self.add_flow(datapath, 2, match, actions)
+
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-	print("DVR In add_flow")
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
@@ -72,16 +118,14 @@ class SimpleSwitchPC(app_manager.RyuApp):
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
 
+	
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         # If you hit this you might want to increase
         # the "miss_send_length" of your switch
-	noflow = 0
-	print("")
-	print("DVR In Packet Handler")
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
+#        if ev.msg.msg_len < ev.msg.total_len:
+ #           self.logger.debug("packet truncated: only %s of %s bytes",
+  #                            ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
@@ -90,40 +134,54 @@ class SimpleSwitchPC(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
-#        v4 = pkt.get_protocol(ipv4.ipv4)
 
-#	if v4.proto == 17:
-#		print("DVR: This is udp protocol")
-	up = pkt.get_protocol(udp.udp)
-	if up.dst_port == 53:
-		print("DVR: HURRAY ...   This is dns packet")
-		print pkt.payload
-#		print up.payload
-		noflow = 1
-	else:
-		#print("DVR: This is not dns packet")
-		print up.dst_port
-#	elif v4.proto == 6:
-#		print("DVR: This is tcp protocol")
-#	else:
-#		print("DVR: This is unknown protocol ")
-#		print(v4.proto)
 		
-		#up = pkt.get_protocols(udp.udp)[0]
-	#up = pkt.get_protocols(udp.udp)[0]
-			
-				
-
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
             return
         dst = eth.dst
         src = eth.src
 
+	udppkt = pkt.get_protocol(udp.udp)
+	if udppkt != None and udppkt.dst_port ==53:
+		time = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+		final_arr=  msg.data[14+20+8+13:msg.total_len-5]
+		#check if the mac is matching to any of risks
+		for x in child_mac:
+			if x == src:
+				for y in blocklist:
+					if final_arr.find(y) >= 0:
+						fromaddr = 'sdnhackathon1@gmail.com'
+						username = 'sdnhackathon1'
+						passwd = 'hackathon1'
+
+						email_msg = MIMEMultipart()
+						email_msg['From'] = fromaddr 
+						email_msg['Subject'] = 'ZODIAC ADMIN : Blacklist ALERT ' + x
+						message = time + ": Blocked " + x + " from " + y
+						email_msg.attach(MIMEText(message))
+
+						server = smtplib.SMTP('smtp.gmail.com:587')
+						server.ehlo()
+						server.starttls()
+						server.ehlo()
+						server.login(username,passwd)
+						for z in email:
+							email_msg['To'] = z 
+							server.sendmail(fromaddr,z,email_msg.as_string())
+						server.quit()
+						print "<font size=\"3\" color=\"red\"> " + message + "</font><br>"
+						return
+						
+		for x in blocklist:
+			if final_arr.find(x) >= 0:
+				print time + " user mac: " + src + " browsed blocklisted url " + blocklist + "<br>" 
+
+		print time + " user mac: " + src + " browsed " + final_arr + "<br>" 
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        #self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+#        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
@@ -134,20 +192,17 @@ class SimpleSwitchPC(app_manager.RyuApp):
             out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
-	print "Actions "	
-	print actions
 
-	        # install a flow to avoid packet_in next time
-	if noflow == 0:
-		if out_port != ofproto.OFPP_FLOOD:
-	 		match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
             # verify if we have a valid buffer_id, if yes avoid to send both
             # flow_mod & packet_out
-			if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-				self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-				return
-			else:
-				self.add_flow(datapath, 1, match, actions)
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
